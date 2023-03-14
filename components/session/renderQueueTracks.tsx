f5c3c88b1d3f69c2dcb9109a05374ddb80aa88b0
Ethan Tyler <38672607+zonedv2@users.noreply.github.com>
2023-03-14 15:31:45 +0000

    fixed queue bug and updated token

--- components/session/renderQueueTracks.tsx
+++ components/session/renderQueueTracks.tsx
@@ -3,11 +3,17 @@ import QueueTrack from './queueTrack';
 import { Stack } from '@chakra-ui/react';
 
 const RenderQueueTracks = ({tracks}) => {
+  // Removes duplicate tracks, Spotify has a bug with playback where it incorrectly adds duplicates to the queue
+  tracks = tracks.filter((value: { uri: String; }, index: Number, self: any[]) =>
+    index === self.findIndex((track) => (
+      track.uri === value.uri
+    ))
+  )
   return (
     <>
-      <Stack spacing={3} mt="2vh"  >
+      <Stack spacing={3} mt="2vh">
         {tracks?.map((track: { id: Key; })  => (
-          <QueueTrack track={track} key={Math.random()*100}/>
+          <QueueTrack track={track} key={track.id}/>
         ))}
       </Stack>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home(props: { client_id: string }) {
   const prod_url = 'https://shared-queue.vercel.app'
   const auth_endpoint = 'https://accounts.spotify.com/authorize'
   const response_type = 'token'
-  const REDIRECT_URI = `${dev_url}/callback&scope=user-read-currently-playing+user-modify-playback-state`
+  const REDIRECT_URI = `${dev_url}/callback&scope=user-read-currently-playing+user-modify-playback-state+user-read-playback-state`
   
   return (
     <Container mt='15vh'>
